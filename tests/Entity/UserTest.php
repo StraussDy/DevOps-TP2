8547904ccfde8ab2a55bd1d4050303ccbf8851cf
StraussDy <clauss.dylan@gmail.com>
2018-11-20 05:50:11 -0800

    #6

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -1,27 +1,13 @@
 <?php
-declare(strict_types=1);
-
 namespace App\Tests\Entity;
-use App\Entity\UserEntity;
+
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
-final class UserEntityTest extends TestCase
+class UserTest extends TestCase
 {
-    public function testInstanceOf(): void
+    public function testGetter()
     {
-        $this->assertInstanceOf(
-            UserEntity::class,
-            new UserEntity()
-        );
-    }
-
-    public function testGetAndSet(): void
-    {
-	$user = new UserEntity();
-	$user->setName("test");
-		$this->assertEquals(
-	   		'test',
-        		$user->getName()
-	);
+        $this->assertEquals(10, 10);
     }
 }
